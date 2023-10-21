b653b1d99c39d40ea5e1d0e935a9ddcf26678551
JonneSaloranta <saloranta.jonne@gmail.com>
2023-10-21 15:15:03 +0300

    added custom login

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('refuel.urls', 'refuel'), namespace='refuel')),
+    path('', include('custom_login.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
