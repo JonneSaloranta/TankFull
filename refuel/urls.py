91317a330763cf0e8bfb85bd217907d42e2248e5
JonneSaloranta <saloranta.jonne@gmail.com>
2023-11-04 12:56:13 +0200

    added favicon path

--- refuel/urls.py
+++ refuel/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('', views.index, name='index'),
     path('features/', views.features, name='features'),
     path('user/<int:user_id>/', views.user_profile, name='user_profile'),
