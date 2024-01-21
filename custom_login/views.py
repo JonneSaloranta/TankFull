efe0049a2c29ffa1b7252a0b942cfff46cdeac32
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-21 19:14:42 +0200

    Add email activation feature to user registration

--- custom_login/views.py
+++ custom_login/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from .forms import UserLoginForm, UserRegisterForm
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage, get_connection
+from decouple import config
+from custom_login.email_activation import activateEmail, activate
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -13,10 +23,18 @@ def login_view(request):
         password = form.cleaned_data.get("password")
         
         user = authenticate(email=email, password=password)
-        login(request, user)
-        if next:
-            return redirect(next)
-        return redirect("/")
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                if next:
+                    return redirect(next)
+                return redirect("/")
+            else:
+                messages.error(request, 'Your account is not active. Please check your email for activation instructions.')
+        else:
+            messages.error(request, 'Invalid email or password.')
+
     context = {
         'form': form,
     }
@@ -27,17 +45,18 @@ def register_view(request):
         return redirect("/")
     next = request.GET.get('next')
     form = UserRegisterForm(request.POST or None)
-    print(request.POST)
-    print(form.errors)
-    print(form.is_valid())
+    
     if form.is_valid():
         user = form.save(commit=False)
         password = form.cleaned_data.get("password")
         user.set_password(password)
+        user.is_active = False
         user.save()
         
-        new_user = authenticate(email=user.email, password=password)
-        login(request, new_user)
+        print(f"{user}, {form.cleaned_data.get('email')}")
+        activateEmail(request, user, form.cleaned_data.get("email"))
+        messages.success(request, "We have sent you an email. Please confirm your email address to complete registration.")
+
         if next:
             return redirect(next)
         return redirect("/")
@@ -49,4 +68,5 @@ def register_view(request):
 
 def logout_view(request):
     logout(request)
+    messages.success(request, 'You have been successfully logged out.')
     return redirect("/")
