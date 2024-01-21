b5d4c6e887aac5559e274b1d76efed9c749b49e1
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-21 19:15:24 +0200

    Add redirect to login page and translate strings

--- refuel/views.py
+++ refuel/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
 
-    # stats.avg_consumption
-    # stats.fuel_consumed
-    # stats.total_distance
-    # stats.total_cost
+import os
+from django.conf import settings
+from django.http import JsonResponse
+from django.core.mail import EmailMessage, get_connection
+
+from decouple import config
+
 
 stats = [
     {
@@ -43,18 +47,18 @@ features_list = [
             'icon': 'bi bi-people'
         },
         {
-            'name': 'Share',
-            'description': 'Share your refuels with others.',
+            'name': _('Share'),
+            'description': _('Share your refuels with others.'),
             'icon': 'bi bi-share'
         },
         {
-            'name': 'Export',
-            'description': 'Export your refuels to a file.',
+            'name': _('Export'),
+            'description': _('Export your refuels to a file.'),
             'icon': 'bi bi-file-earmark-arrow-down'
         },
         {
-            'name': 'Import',
-            'description': 'Import your refuels from a file.',
+            'name': _('Import'),
+            'description': _('Import your refuels from a file.'),
             'icon': 'bi bi-file-earmark-arrow-up'
         },
     ]
@@ -77,11 +81,11 @@ def features(request):
 from email_login.models import User
 from .models import Vehicle, Refuel
 
-@login_required(login_url='/login/')
+@login_required()
 def user_profile(request, user_id):
     user = User.objects.filter(id=user_id).first()
     vehicles = Vehicle.objects.filter(user=user)
-    refuels = Refuel.objects.filter(user=user)
+    refuels = Refuel.objects.all().order_by('-id')
 
     context = {
         'user': user,
