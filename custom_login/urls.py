5865ec7466f937b58efc49419c87d78a665d1ef7
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-21 19:13:45 +0200

    Add email activation path to urls.py

--- custom_login/urls.py
+++ custom_login/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate_email'),
 ]
