d3107ef98503b721ec0e23e4e8036afc79acc21c
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-24 22:46:28 +0200

    Remove i18n_patterns from urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -19,11 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import debug_toolbar
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
+# urlpatterns = i18n_patterns(
+
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('refuel.urls', 'refuel'), namespace='refuel')),
     path('', include('custom_login.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
