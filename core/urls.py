04baa62f7887f304ef3591abd40572ccbb2a3aa2
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-24 14:09:43 +0200

    Add debug toolbar URL path to core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('refuel.urls', 'refuel'), namespace='refuel')),
     path('', include('custom_login.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
