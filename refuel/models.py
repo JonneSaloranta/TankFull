77fdcab33b8b95f2201073bc26f1ffec9a055f59
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-25 22:42:48 +0200

    Refactor Refuel model and add default values for date and user

--- refuel/models.py
+++ refuel/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from core.settings import AUTH_USER_MODEL as User
-
-# Create your models here.
+from django.utils import timezone
 
 class FuelType(models.Model):
     fuel_type = models.CharField(max_length=100, primary_key=True, unique=True)
@@ -26,20 +25,20 @@ class Vehicle(models.Model):
 class Refuel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(null=True, blank=True)
     odometer = models.IntegerField()
     fuel_amount = models.DecimalField(max_digits=6, decimal_places=2)
-    cost = models.DecimalField(max_digits=6, decimal_places=2)
-    cost_per_unit = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    units_per_distance = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    notes = models.CharField(max_length=200, null=True, blank=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=3)
 
     def __str__(self):
         return f'{self.vehicle} - {self.date}'
     
     def save(self, *args, **kwargs):
-        self.cost_per_unit = self.cost / self.fuel_amount
-        self.units_per_distance = self.fuel_amount / self.odometer
+        if not self.date:
+            self.date = timezone.now()
+        if not self.user:
+            self.user = self.vehicle.user
+        self.vehicle.save()
         super(Refuel, self).save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
