64bd1bd788cc19c6f56a133d266a2f885066304a
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-25 23:32:53 +0200

    Add API endpoints for user, vehicle, and refuel details

--- refuel/urls.py
+++ refuel/urls.py
@@ -9,5 +9,14 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('features/', views.features, name='features'),
     path('user/<int:user_id>/', views.user_profile, name='user_profile'),
+    ## API
     path('user/<int:user_id>/vehicles/', views.user_vehicles, name='user_vehicles'),
+    path('user/<int:user_id>/details/', views.user_details, name='user_details'),
+    path('vehicle/<int:vehicle_id>/details/', views.vehicle_details, name='vehicle_details'),
+    path('refuel/<int:refuel_id>/details/', views.refuel_details, name='refuel_details'),
+    path('vehicle/<int:vehicle_id>/refuels/', views.vehicle_refuels, name='vehicle_refuels'),
+    path('user/<int:user_id>/refuels/', views.user_refuels, name='user_refuels'),
+    path('vehicle/<int:vehicle_id>/update/', views.update_vehicle, name='update_vehicle'),
+    path('vehicle/create/', views.create_vehicle, name='create_vehicle'),
+    path('fuel_types/', views.get_fuel_types, name='get_fuel_types'),
 ]
