5a715d09047e510508b396f5b98456c16305d6a5
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-28 17:03:01 +0200

    Add VehicleImageForm to refuel/forms.py

--- refuel/forms.py
+++ refuel/forms.py
@@ -1,4 +1,4 @@
-from .models import Vehicle, Refuel, FuelType
+from .models import Vehicle, Refuel, FuelType, VehicleImage
 from django import forms
 
 class VehicleForm(forms.ModelForm):
@@ -7,11 +7,23 @@ class VehicleForm(forms.ModelForm):
         fields = '__all__'
 
 class RefuelForm(forms.ModelForm):
+
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    odometer = forms.IntegerField()
+    fuel_amount = forms.DecimalField(max_digits=6, decimal_places=2)
+    cost = forms.DecimalField(max_digits=6, decimal_places=2)
     class Meta:
         model = Refuel
-        fields = '__all__'
+        fields = 'vehicle', 'date', 'odometer', 'fuel_amount', 'cost'
+
 
 class FuelTypeForm(forms.ModelForm):
     class Meta:
         model = FuelType
+        fields = '__all__'
+
+
+class VehicleImageForm(forms.ModelForm):
+    class Meta:
+        model = VehicleImage
         fields = '__all__'
